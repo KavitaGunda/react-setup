96e2614bc6a1c7cc8c44b50ba94672c2cef763d1
Kavita Gunda <gundakavita@gmail.com>
2022-01-27 12:44:40 +0530

    Test Husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import './Styles.css'
+import IMAGE from './react.png'
+import SVG from './svgfile.svg'
+import { ClickCounter } from './ClickCounter'
+
 export const App = () => {
-  return <h1> React Setup </h1>
+  const pro = 'Gunda'
+  return (
+    <>
+      <h1>
+        {' '}
+        React Setup - {pro} {process.env.NODE_ENV} {process.env.name}{' '}
+      </h1>
+      <img src={IMAGE} alt="React Logo" width={300} height={200} />
+      <img src={SVG} alt="React SVG" width={300} height={200} />
+      <ClickCounter />
+    </>
+  )
 }
